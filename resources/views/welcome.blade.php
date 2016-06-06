a7a78839e998c768586d5bfdd59831f35cbc9c2b
Lean Karlo Corpuz <lean@teknolohiya.ph>
2016-06-06 15:46:00 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 .2</div>
+                <div class="title">STAGING BOARDS API</div>
             </div>
         </div>
     </body>
