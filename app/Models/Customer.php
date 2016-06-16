61ca5197763583a4ae8d555190376cf120bbd10c
Lean Karlo Corpuz <lean@teknolohiya.ph>
2016-06-16 19:20:12 +0800

    fix for missing model user

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -17,7 +17,7 @@ class Customer extends Model
 
     public function user()
     {
-    	return $this->hasOne('App\Models\user', 'id', 'user_id');
+    	return $this->hasOne('App\Models\User', 'id', 'user_id');
     }
 
     public function order()
